c217a518edaff1b57d65b4af5e030e930dd1f776
sivaselagala <sivaselagala85@gmail.com>
2022-09-15 00:18:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var axios = require('axios');
+var FormData = require('form-data');
+var fs = require('fs');
+var data = new FormData();
+data.append('changeLogZip', fs.createReadStream('liquibase.zip'));
+
+tenant = process.env.TENANT
+username = process.env.USERNAME
+pwd = process.env.PASSWORD
+
+var config = {
+  method: 'post',
+  url: 'https://1750.callidusondemand.com/TrueComp-SaaS/services/rest/touchlessdeployment/update?changeMasterFile=TouchLess.xml',
+  auth: {
+  username: username,
+  password: pwd,
+  },
+  headers: { 
+    ...data.getHeaders()
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
